473bcc9588ce3bae085d1e1d0745c001831fcd3a
prasath s <pras@gmail>
2025-01-13 11:51:01 +0530

    blockimage

--- miniproject/img_gallery/gallery/admin.py
+++ miniproject/img_gallery/gallery/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from.models import blogger,post
 
 # Register your models here.
+class BookAdmin(admin.ModelAdmin):
+    list_display=('first_name','last_name','email','contact')
+#     list_display=(' title','date','content')
+admin.site.register(blogger,BookAdmin)
+admin.site.register(post)
--- miniproject/img_gallery/gallery/models.py
+++ miniproject/img_gallery/gallery/models.py
@@ -7,3 +7,17 @@ class Image(models.Model):
     
     def __str__(self):
         return self.title
+class blogger(models.Model):
+    first_name=models.CharField(max_length=20)
+    last_name=models.CharField(max_length=20)
+    email=models.CharField(max_length=20)
+    contact=models.IntegerField()
+    def __str__(self):
+      return f'name:{self.first_name} age{self.last_name} email{self.email} contact{self.contact}'
+class post(models.Model):
+    title=models.CharField(max_length=50)
+    date=models.IntegerField()
+    content=models.CharField(max_length=50)
+    blogger=models.ForeignKey('blogger',on_delete=models.CASCADE)
+    def __str__(self):
+        return f'Title{self.title} date{self.rating} content{self.content}' 
